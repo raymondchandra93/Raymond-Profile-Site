2c1145b501defa9f1adbe1d5986415f0647928c3
Raymond Chandra <raymond.schandra@gmail.com>
2025-05-02 11:58:28 -0400

    Creating a new React project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './header'
+
+function App() {
+  return (
+    <>
+      <div id="header" className='container-sm'>
+        <Header />
+      </div>
+      <div id="profile" className='container-sm'>
+        <img src='/assets/raymond_1.jpg'></img>
+      </div>
+    </>
+  )
+}
+
+export default App
